153c980e42424bbd29f99514030edfcef7b045f0
bryan.yang <bryan.yang@ringcentral.com>
2016-09-14 17:44:50 +0800

    add chat bot

--- handlers/yo.js
+++ handlers/yo.js
@@ -1,5 +1,6 @@
 var _ = require('underscore'),
-	fast_bindall = require('fast_bindall');
+    sbot = require('sbot'),
+    fast_bindall = require('fast_bindall');
 
 Array.prototype.subarray=function(start,end){
      if(!end){ end=-1;} 
@@ -13,6 +14,7 @@ var Yo = function(options) {
 		throw "Handler requires bot";
 	}
 	this.bot.on('message', this.handle_message);
+    this.sbot = new sbot();
 };
 
 _.extend(Yo.prototype, {
@@ -23,13 +25,24 @@ _.extend(Yo.prototype, {
 			var text = data.text;
 			var test = text.match(/^\!yo (.*?)$/i);
 			if (test && test.length > 0) {
-				var content = test.subarray(1,-1);
+				var content = test.subarray(1,-1).join(" ");
 				var self = this;
+                var url = "http://localhost:5000/bot/api/v1.0/" + encodeURIComponent(content);
+                var self = this;
+                this.sbot.fetch_object(url, {}, function(error, result) {
+                    if (error || !result || !result.res ) {
+                        console.warn(error);
+                        return self.bot.post(data.group_id, "Bot is missing");
+                    }
+                    var text = result.res;
+                    return self.bot.post(data.group_id, text);
 					// maybe use first.forecast at some point
-				return self.bot.post(data.group_id, "Yo");
-				};
-			}
-		}
+				   //return self.bot.post(data.group_id, "Yo");
+                ;
+            })
+        }
+    }
+}
 });
 
 module.exports = Yo;
